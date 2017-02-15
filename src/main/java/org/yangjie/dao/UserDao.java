1aae74952678cf23585b7cf94ac497aa4f269c11
yangjie <yangjie@moko.cc>
2017-02-15 14:55:31 +0800

    添加插入方法

--- src/main/java/org/yangjie/dao/UserDao.java
+++ src/main/java/org/yangjie/dao/UserDao.java
@@ -1,8 +1,10 @@
 package org.yangjie.dao;
 
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.SelectKey;
 import org.yangjie.entity.User;
 
 @Mapper // 注解为mybatis的Mapper, 自动生成代理实现
@@ -10,5 +12,9 @@ public interface UserDao {
 	
 	@Select("select * from user where username=#{username} and password=#{password}")
 	public User select(@Param("username")String username, @Param("password")String password);
+	
+	@Insert("insert into user (username, password) values (#{username}, #{password})")
+	@SelectKey(keyProperty="userid",statement="SELECT LAST_INSERT_ID()", before=false, resultType=Integer.class)  
+	public boolean insert(User user);
 	
 }
